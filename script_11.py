9936224d23fa997cd0bbcd38991f36195f7f122a
Emiliabz <emiliabrbz@gmail.com>
2026-01-29 17:31:02 -0300

    Add scripts

---
+++ script_11.py
@@ -0,0 +1,6 @@
+﻿real = float(input('Quanto dinheiro vocÃª tem na carteira? R$'))
+dolar = real / 5.36
+euro = real / 6.28
+print('Com R${} vocÃª pode comprar U$${} e EUR{}'.format(real, dolar, euro))
+
+#Tranformando real em dolar e euro
